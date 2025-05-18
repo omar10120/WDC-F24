bec228134f51f914c563e44b210124071789b4ed
tamer tamer <amr.dawoodi@hotmail.com>
2025-05-18 11:11:01 +0300

    first commit

---
+++ WDC-F24.infrastructure/Data/AppDbContext .cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WDC_F24.infrastructure.Data;
+
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using System.Text;
+
+
+
+namespace WDC_F24.infrastructure.Data
+{
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+            //public DbSet<Product> Products { get; set; }
+
+            // You can override OnModelCreating if needed
+        }
+}
---
+++ WDC-F24.infrastructure/DependencyInjection .cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WDC_F24.infrastructure.Data;
+
+
+
+
+
+namespace WDC_F24.infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            
+
+            return services;
+        }
+    }
+}
